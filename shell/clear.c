6b21c5a22b806ea7b83c50b68a5e28b5958cd6f9
Anthony Gargiulo <anthony@agargiulo.com>
2013-04-10 19:43:42 -0400

    Clear actually clears the screen now

--- shell/clear.c
+++ shell/clear.c
@@ -39,4 +39,11 @@
 void run_clear( int argc, char **argv ) {
 	c_setscroll( 0, 7, 99, 99 );
 	c_puts_at( 0, 6, "================================================================================" );
+	for (uint8_t x = 0; x < 99; ++x)
+	{
+		for (uint8_t y = 7; y < 99; ++y)
+		{
+			c_putchar_at(x, y, ' ');
+		}
+	}
 }
