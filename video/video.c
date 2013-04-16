9c5b3e16bf25ae1271b8313e016dfea8b0d04190
Alex Walcutt <alexwalcutt@csh.rit.edu>
2013-04-16 15:30:00 -0400

    Fixed some assembly stuff

--- video/video.c
+++ video/video.c
@@ -4,6 +4,9 @@
 
 void _video_test( void )
 {
+	c_puts("Entering Video Test...\n");
 	_video_set_mode_13();
+	c_puts("Video Set Mode Complete\n");
 	_video_plot_pixel_test();
+	c_puts("Plot Pixel Test Complete\n");
 }
--- video/video_s.s
+++ video/video_s.s
@@ -6,24 +6,24 @@
 .globl _video_plot_pixel_test
 
 _video_set_mode_13:
-	xorl %eax, %eax
-	mov  %al, 0x13
-	int $0x10
+	xorl %eax,  %eax
+	mov  $0x13, %al
+	int  $0x10
 	ret
 
 _video_set_mode_text:
-	xorl %eax, %eax
-	mov %al, 0x03
-	int $0x10
+	xorl %eax,  %eax
+	mov  $0x03, %al
+	int  $0x10
 	ret
 
 _video_plot_pixel_test:
-	xorl %eax, %eax
-	xorl %ecx, %ecx
-	xorl %edx, %edx
-	mov  %ah, 0x0C
-	mov  %al, 0x02
-	mov  %cx, 10
-	mov  %dx, 10
-	int $0x10
+	xorl %eax,  %eax
+	xorl %ecx,  %ecx
+	xorl %edx,  %edx
+	mov  $0x0C, %ah
+	mov  $0x02, %al
+	mov  $10,   %cx
+	mov  $10,   %dx
+	int  $0x10
 	ret
