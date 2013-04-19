b5ef96c968d0df40f04026796f84d554cd2b5a5f
Anthony Gargiulo <anthony@agargiulo.com>
2013-04-19 15:23:41 -0400

    added a command to dump the PCI config space of the network card

--- network/8255x.c
+++ network/8255x.c
@@ -1,19 +1,14 @@
 /*
-<<<<<<< HEAD
  * File: network/8255x.c
  *
  * Author: Anthony Gargiulo (20123)
  *
  * Description: Driver for the Intel Pro 100 8255x (rev 8) card
-=======
- * File:	?
+ * File: network/8255x.c
  *
- * Author:	4003-506 class of 20123
+ * Author: Anthony Gargiulo (20123)
  *
- * Contributor:
- *
- * Description:	?
->>>>>>> Basic things for the 8255x card, and a few other minor changes
+ * Description: Driver for the Intel Pro 100 8255x (rev 8) card
  */
 
 #define	__SP2_KERNEL__
