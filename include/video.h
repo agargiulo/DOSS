179cfe7fd96a7178bbe2bc3fdaf4d91bd6e45532
Alex Walcutt <alexwalcutt@csh.rit.edu>
2013-05-07 15:34:27 -0400

    COLORS WOOO :D

--- include/video.h
+++ include/video.h
@@ -29,6 +29,26 @@
 ** Types
 */
 
+typedef enum Color
+{
+	BLACK,
+	BLUE,
+	GREEN,
+	CYAN,
+	RED,
+	MAGENTA,
+	BROWN,
+	LIGHTGRAY,
+	DARKGRAY,
+	LIGHTBLUE,
+	LIGHTGREEN,
+	LIGHTCYAN,
+	LIGHTRED,
+	LIGHTMAGENTA,
+	YELLOW,
+	WHITE
+} Color;
+
 typedef struct point {int x,y;} point;
 
 /*
@@ -40,10 +60,10 @@ typedef struct point {int x,y;} point;
 */
 
 void _video_test( void );
-void _video_point(int x, int y);
-void _video_line(point *p1, point *p2);
-void _video_box(point *p1, point *p2);
-void _video_box_filled(point *p1, point *p2);
+void _video_point(int x, int y, enum Color c);
+void _video_line(point *p1, point *p2, enum Color c);
+void _video_box(point *p1, point *p2, enum Color c);
+void _video_box_filled(point *p1, point *p2, enum Color c);
 
 #endif
 
--- video/video.c
+++ video/video.c
@@ -14,30 +14,35 @@ void _video_test( void )
 		}
 	}*/
 
+	enum Color c;
+
 	//_video_line test
 	point p1 = {20,20};
 	point p2 = {20,120};
-	_video_line(&p1, &p2);
+	c = LIGHTMAGENTA;
+	_video_line(&p1, &p2, c);
 
 	//_video_box test
 	point p3 = {30,30};
 	point p4 = {40,40};
-	_video_box(&p3, &p4);
+	c = LIGHTCYAN;
+	_video_box(&p3, &p4, c);
 
 	//_video_box_filled test
 	point p5 = {150,50};
 	point p6 = {300,150};
-	_video_box_filled(&p5, &p6);
+	c = YELLOW;
+	_video_box_filled(&p5, &p6, c);
 }
 
-void _video_point(int x, int y)
+void _video_point(int x, int y, enum Color c)
 {
 	int offset = (y * 320) + x;
 	unsigned char *pixel = (unsigned char*) 0xa0000;
-	pixel[offset] = 13;
+	pixel[offset] = (int) c;
 }
 
-void _video_line(point *p1, point *p2)
+void _video_line(point *p1, point *p2, enum Color c)
 {
 	point top,bot;
 	double deltaX,x;
@@ -60,14 +65,14 @@ void _video_line(point *p1, point *p2)
 		{
 			for(i = top.x; i <= bot.x; i++)
 			{
-				_video_point(i, top.y);
+				_video_point(i, top.y, c);
 			}
 		}
 		else
 		{
 			for(i = bot.x; i <= top.x; i++)
 			{
-				_video_point(i, top.y);
+				_video_point(i, top.y, c);
 			}
 		}
 		
@@ -80,33 +85,33 @@ void _video_line(point *p1, point *p2)
 	for(i = top.y; i <= bot.y; i++)
 	{
 		ix = x;
-		_video_point(ix, i);
+		_video_point(ix, i, c);
 		x += deltaX;
 	}
 
 	return;
 }
 
-void _video_box(point *p1, point *p2)
+void _video_box(point *p1, point *p2, enum Color c)
 {
 	point corner1 = {p1->x, p1->y};
 	point corner2 = {p2->x, p1->y};
 	point corner3 = {p2->x, p2->y};
 	point corner4 = {p1->x, p2->y};
 
-	_video_line(&corner1, &corner2);
-	_video_line(&corner2, &corner3);
-	_video_line(&corner3, &corner4);
-	_video_line(&corner4, &corner1);
+	_video_line(&corner1, &corner2, c);
+	_video_line(&corner2, &corner3, c);
+	_video_line(&corner3, &corner4, c);
+	_video_line(&corner4, &corner1, c);
 }
 
-void _video_box_filled(point *p1, point *p2)
+void _video_box_filled(point *p1, point *p2, enum Color c)
 {
 	for(int x = p1->x; x <= p2->x; x++) 
 	{
 		point start = {x, p1->y};
 		point end = {x, p2->y};
-		_video_line(&start, &end);
+		_video_line(&start, &end, c);
 	}
 }
 
