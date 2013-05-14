52dd5564dfffc51181841be3c066e305f89b0533
Anthony Gargiulo <anthony@agargiulo.com>
2013-05-14 12:35:39 -0400

    net header file

---
+++ include/net.h
@@ -0,0 +1,41 @@
+/*
+** File: include/net.h
+**
+** Author: Anthony Gargiulo
+**
+** Description: Kernel level network API
+*/
+
+#ifndef _NET_H_
+#define _NET_H_
+
+#include <common.h>
+#include <8255x.h>
+
+
+/*
+** General (C and/or assembly) definitions
+*/
+
+#ifndef __SP2_ASM__
+
+/*
+** Start of C-only definitions
+*/
+
+/*
+** Types
+*/
+
+/*
+** Globals
+*/
+
+/*
+** Prototypes
+*/
+
+
+#endif
+
+#endif
